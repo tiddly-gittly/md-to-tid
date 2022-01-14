35ed94083543c64b7d5f356789fa77d607021d0e
linonetwo <linonetwo@users.noreply.github.com>
2022-01-14 05:57:22 +0000

    deploy: 1eaae9bd5437b4c47cc0e885602477172e22e698

--- bundle.js
+++ bundle.js
@@ -60203,20 +60203,11 @@
 	  return value;
 	}
 
-	/**
-	 * @typedef {import('../types').Context} Context
-	 * @typedef {import('../types').Options} Options
-	 */
-
-	/**
-	 * @param {Context} context
-	 * @returns {Exclude<Options['emphasis'], undefined>}
-	 */
 	function checkEmphasis(context) {
-	  const marker = context.options.emphasis || '*';
+	  const marker = context.options.emphasis ?? `''`;
 
-	  if (marker !== '*' && marker !== '_') {
-	    throw new Error('Cannot serialize emphasis with `' + marker + '` for `options.emphasis`, expected `*`, or `_`');
+	  if (marker !== `''` && marker !== '_') {
+	    throw new Error(`Cannot serialize emphasis with ${marker} for options.emphasis, expected  \`''\`, or \`_\``);
 	  }
 
 	  return marker;
@@ -60286,20 +60277,7 @@
 	  return results.join('');
 	}
 
-	/**
-	 * @typedef {import('mdast').Emphasis} Emphasis
-	 * @typedef {import('../types').Handle} Handle
-	 */
-	emphasis$1.peek = emphasisPeek; // To do: there are cases where emphasis cannot “form” depending on the
-	// previous or next character of sequences.
-	// There’s no way around that though, except for injecting zero-width stuff.
-	// Do we need to safeguard against that?
-
-	/**
-	 * @type {Handle}
-	 * @param {Emphasis} node
-	 */
-
+	emphasis$1.peek = emphasisPeek;
 	function emphasis$1(node, _, context) {
 	  const marker = checkEmphasis(context);
 	  const exit = context.enter('emphasis');
@@ -60316,7 +60294,7 @@
 	 */
 
 	function emphasisPeek(_, _1, context) {
-	  return context.options.emphasis || '*';
+	  return context.options.emphasis ?? `''`;
 	}
 
 	function heading$1(node, _, context) {
