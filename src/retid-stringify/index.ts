960f8581e9adf408ac079f8f6884651e0cdb66d3
林东吴 <lindongwu@bytedance.com>
2022-01-16 16:58:36 +0800

    feat: pass settings

--- src/retid-stringify/index.ts
+++ src/retid-stringify/index.ts
@@ -1,12 +1,13 @@
 import { Options as IToMarkdownOptions, toTid } from '../tidast-util-to-tid';
 import { Plugin, Processor, CompilerFunction } from 'unified';
 import { Root as MdastRoot, Content as MdastContent } from 'mdast';
+import { merge } from 'lodash';
 
 type MdastNode = MdastRoot | MdastContent;
 
 export type IOptions = Omit<IToMarkdownOptions, 'extensions'>;
 
-  // @ts-expect-error: bad type written by unified author.
+// @ts-expect-error: bad type written by unified author.
 export const retidStringify: Plugin<[(IOptions | undefined)?] | void[], MdastRoot, string> = function (options) {
   const compiler: CompilerFunction<MdastNode, string> = (tree) => {
     // Assume options.
@@ -14,7 +15,7 @@ export const retidStringify: Plugin<[(IOptions | undefined)?] | void[], MdastRoo
 
     return toTid(
       tree,
-      Object.assign({ bullet: '*', bulletOrdered: '#', incrementListMarker: false }, settings, options, {
+      merge({ bullet: '*', bulletOrdered: '#', incrementListMarker: false }, settings, options, {
         // Note: this option is not in the readme.
         // The goal is for it to be set by plugins on `data` instead of being
         // passed by users.
---
+++ test/escape.test.mjs
@@ -0,0 +1,309 @@
+import { toString, md2tid } from '../dist/index.mjs';
+
+describe('escape', () => {
+  test('should escape what would otherwise be a block quote in a paragraph', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '> a\n> b\nc >' }] })).toEqual('\\> a\n\\> b\nc >\n');
+  });
+
+  test('should escape what would otherwise be a block quote in a list item', () => {
+    expect(
+      toString({
+        type: 'listItem',
+        children: [{ type: 'paragraph', children: [{ type: 'text', value: '> a\n> b' }] }],
+      }),
+    ).toEqual('* \\> a\n* \\> b\n');
+  });
+
+  test('should escape what would otherwise be a block quote in a block quote', () => {
+    expect(
+      toString({
+        type: 'blockquote',
+        children: [{ type: 'paragraph', children: [{ type: 'text', value: '> a\n> b' }] }],
+      }),
+    ).toEqual('> \\> a\n> \\> b\n');
+  });
+
+  test('should escape what would otherwise be a break', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\\\nb' }] })).toEqual('a\\\\\nb\n');
+  });
+
+  test('should escape what would otherwise be a named character reference', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '&amp' }] })).toEqual('\\&amp\n');
+  });
+
+  test('should escape what would otherwise be a numeric character reference', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '&#9;' }] })).toEqual('\\&#9;\n');
+  });
+
+  test('should escape what would otherwise be a character escape', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\\+b' }] })).toEqual('a\\\\+b\n');
+  });
+
+  test('should escape what would otherwise be a character escape of an autolink', () => {
+    expect(
+      toString({
+        type: 'paragraph',
+        children: [
+          { type: 'text', value: 'a\\' },
+          {
+            type: 'link',
+            children: [{ type: 'text', value: 'https://a.b' }],
+            url: 'https://a.b',
+          },
+        ],
+      }),
+    ).toEqual('a\\\\[ext[https://a.b]]\n');
+  });
+
+  test('should escape what would otherwise be code (flow)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '```js\n```' }] })).toEqual('\\`\\`\\`js\n\\`\\`\\`\n');
+  });
+
+  test('should escape what would otherwise be a definition', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '[a]: b' }] })).toEqual('\\[a]: b\n');
+  });
+
+  test('should escape what would otherwise be emphasis (slash)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '//a//' }] })).toEqual('\\/\\/a\\/\\/\n');
+  });
+
+  test('should escape what would otherwise be bold (underscore)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: `''a''` }] })).toEqual(`\\'\\'a\\'\\'\n`);
+  });
+
+  test('should escape what would otherwise be a heading (atx)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '# a' }] })).toEqual('\\# a\n');
+  });
+
+  test('should escape what would otherwise be a heading (setext, equals)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\n=' }] })).toEqual('a\n\\=\n');
+  });
+
+  test('should escape what would otherwise be a heading (setext, dash)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\n-' }] })).toEqual('a\n\\-\n');
+  });
+
+  test('should escape what would otherwise be html', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '<a\nb>' }] })).toEqual('\\<a\nb>\n');
+  });
+
+  test('should escape what would otherwise be code (text)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a `b`\n`c` d' }] })).toEqual('a \\`b\\`\n\\`c\\` d\n');
+  });
+
+  test('should escape what would otherwise turn a link into an image', () => {
+    expect(
+      toString({
+        type: 'paragraph',
+        children: [
+          { type: 'text', value: '!' },
+          {
+            type: 'link',
+            children: [{ type: 'text', value: 'a' }],
+            url: 'b',
+          },
+        ],
+      }),
+    ).toEqual('\\![[a|b]]\n');
+  });
+
+  test('should escape what would otherwise turn a link reference into an image reference', () => {
+    expect(
+      toString({
+        type: 'paragraph',
+        children: [
+          { type: 'text', value: '!' },
+          {
+            type: 'linkReference',
+            children: [{ type: 'text', value: 'a' }],
+            label: 'b',
+            identifier: 'b',
+            referenceType: 'shortcut',
+          },
+        ],
+      }),
+    ).toEqual('\\![a][b]\n');
+  });
+
+  test('should escape what would otherwise be an image (reference)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '![a][b]' }] })).toEqual('!\\[a]\\[b]\n');
+  });
+
+  test('should escape what would otherwise be an image (resource)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '![](a.jpg)' }] })).toEqual('!\\[]\\(a.jpg)\n');
+  });
+
+  test('should escape what would otherwise be a link (reference)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '[a][b]' }] })).toEqual('\\[a]\\[b]\n');
+  });
+
+  test('should escape what would otherwise be a link (resource)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '[](a.jpg)' }] })).toEqual('\\[]\\(a.jpg)\n');
+  });
+
+  test('should escape what would otherwise be a list item (plus)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '+ a\n+ b' }] })).toEqual('\\+ a\n\\+ b\n');
+  });
+
+  test('should escape what would otherwise be a list item (dash)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '- a\n- b' }] })).toEqual('\\- a\n\\- b\n');
+  });
+
+  test('should escape what would otherwise be a list item (asterisk)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '* a\n* b' }] })).toEqual('\\* a\n\\* b\n');
+  });
+
+  test('should escape what would otherwise be a list item (dot)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '1. a\n2. b' }] })).toEqual('1\\. a\n2\\. b\n');
+  });
+
+  test('should escape what would otherwise be a list item (paren)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '1) a\n2) b' }] })).toEqual('1\\) a\n2\\) b\n');
+  });
+
+  test('should not escape what can’t be a list (dot)', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '1.2.3. asd' }] })).toEqual('1.2.3. asd\n');
+  });
+
+  test.skip('should support options in extensions', () => {
+    expect(
+      toString(
+        {
+          type: 'root',
+          children: [
+            { type: 'definition', url: '', label: 'a', identifier: 'a' },
+            { type: 'definition', url: '', label: 'b', identifier: 'b' },
+          ],
+        },
+        { extensions: [{ tightDefinitions: true }] },
+      ),
+    ).toEqual('[a]: <>\n[b]: <>\n');
+  });
+
+  test('should support empty `join`, `handlers`, `extensions` in an extension (coverage)', () => {
+    expect(
+      toString(
+        {
+          type: 'root',
+          children: [{ type: 'strong', children: [{ type: 'text', value: 'a' }] }],
+        },
+        {
+          extensions: [
+            {
+              strong: '_',
+              join: undefined,
+              handlers: undefined,
+              extensions: undefined,
+            },
+          ],
+        },
+      ),
+    ).toEqual(`_a_\n`);
+  });
+
+  test('should make `join` from options highest priority', () => {
+    expect(
+      toString(
+        {
+          type: 'root',
+          children: [
+            {
+              type: 'list',
+              ordered: true,
+              start: 1,
+              spread: false,
+              children: [
+                {
+                  type: 'listItem',
+                  spread: true,
+                  checked: null,
+                  children: [
+                    {
+                      type: 'paragraph',
+                      children: [
+                        {
+                          type: 'text',
+                          value: 'foo',
+                        },
+                      ],
+                    },
+                    {
+                      type: 'list',
+                      ordered: false,
+                      start: null,
+                      spread: false,
+                      children: [
+                        {
+                          type: 'listItem',
+                          spread: false,
+                          checked: null,
+                          children: [
+                            {
+                              type: 'paragraph',
+                              children: [{ type: 'text', value: 'bar' }],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          join: [() => 0],
+        },
+      ),
+    ).toEqual('# foo\n# * bar\n');
+  });
+
+  test('should prefer main options over extension options', () => {
+    expect(
+      toString(
+        {
+          type: 'root',
+          children: [{ type: 'strong', children: [{ type: 'text', value: 'a' }] }],
+        },
+        { strong: `''`, extensions: [{ strong: '_' }] },
+      ),
+    ).toEqual(`''a''\n`);
+  });
+
+  test('should prefer extension options over subextension options', () => {
+    expect(
+      toString(
+        {
+          type: 'root',
+          children: [{ type: 'strong', children: [{ type: 'text', value: 'a' }] }],
+        },
+        { extensions: [{ strong: `''`, extensions: [{ strong: '_' }] }] },
+      ),
+    ).toEqual(`''a''\n`);
+  });
+
+  test('should handle literal backslashes properly when before constructs (1)', () => {
+    expect(
+      toString({
+        type: 'paragraph',
+        children: [
+          { type: 'text', value: '\\' },
+          { type: 'emphasis', children: [{ type: 'text', value: 'a' }] },
+        ],
+      }),
+    ).toEqual('\\\\//a//\n');
+  });
+
+  test('should handle literal backslashes properly when before constructs (2)', () => {
+    expect(
+      toString({
+        type: 'paragraph',
+        children: [
+          { type: 'text', value: '\\\\' },
+          { type: 'emphasis', children: [{ type: 'text', value: 'a' }] },
+        ],
+      }),
+    ).toEqual('\\\\\\\\//a//\n');
+  });
+});
