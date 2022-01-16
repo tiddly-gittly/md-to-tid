289f137a618488247c594a78000aca680f0a39b4
林东吴 <lindongwu@bytedance.com>
2022-01-16 16:29:48 +0800

    test: paragraph

---
+++ test/paragraph.test.mjs
@@ -0,0 +1,35 @@
+import { toString, md2tid } from '../dist/index.mjs';
+
+describe('paragraph', () => {
+  test('should support an empty paragraph', () => {
+    expect(toString({ type: 'paragraph' })).toEqual('');
+  });
+
+  test('should support a paragraph', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\nb' }] })).toEqual('a\nb\n');
+  });
+
+  test('should encode spaces at the start of paragraphs', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '  a' }] })).toEqual('&#x20; a\n');
+  });
+
+  test('should encode spaces at the end of paragraphs', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a  ' }] })).toEqual('a &#x20;\n');
+  });
+
+  test('should encode tabs at the start of paragraphs', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: '\t\ta' }] })).toEqual('&#x9;\ta\n');
+  });
+
+  test('should encode tabs at the end of paragraphs', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\t\t' }] })).toEqual('a\t&#x9;\n');
+  });
+
+  test('should encode spaces around line endings in paragraphs', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a  \n  b' }] })).toEqual('a &#x20;\n&#x20; b\n');
+  });
+
+  test('should encode spaces around line endings in paragraphs', () => {
+    expect(toString({ type: 'paragraph', children: [{ type: 'text', value: 'a\t\t\n\t\tb' }] })).toEqual('a\t&#x9;\n&#x9;\tb\n');
+  });
+});
