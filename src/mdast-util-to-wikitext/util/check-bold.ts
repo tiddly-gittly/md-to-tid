c9eec0027ff8ddae7eaa8481127941667b084554
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-03-20 14:52:11 +0000

    change bold comment

--- src/mdast-util-to-wikitext/util/check-bold.ts
+++ src/mdast-util-to-wikitext/util/check-bold.ts
@@ -4,7 +4,7 @@ import { Options, State } from '../types';
  * 检查并返回用于表示强调文本的标记。
  *
  * @param {State} state - 包含配置选项的状态对象。
- * @returns {Exclude<Options['bold'], null | undefined>} - 用于表示强调文本的标记，必须是 `*` 或 `_`。
+ * @returns {Exclude<Options['bold'], null | undefined>} - 用于表示强调文本的标记，必须是 `''`。
  */
 export function checkBold(state: State): Exclude<Options['bold'], null | undefined> {
   const marker = state.options.bold || `''`;
